7f1e715d5fbec52e28688795ef8fd20d366b6c2a
Galen Sho Sato <galen.sato@gmail.com>
2023-03-23 13:58:42 -0400

    Completed Lab

--- javascript/roman_numeral.js
+++ javascript/roman_numeral.js
@@ -1,5 +1,84 @@
+// My Code
 function romanNumeral(string) {
-  // type your code here
+  let number = 0
+  let number_array = string.split('')
+  
+  function helper(array){
+    if (array.length === 0) {
+      return number
+    }
+    let input = array[0]
+
+    switch (input) {
+      case 'M':
+        number = number + 1000
+        array = array.splice(1)
+        break;
+      case 'D':
+        number = number + 500
+        array = array.splice(1)
+        break;
+      case 'C':
+        if (array[1]=== 'M') {
+          number = number + 900
+          array = array.splice(2)
+        }
+        else if (array[1] === 'D'){
+          number = number + 400
+          array = array.splice(2)
+        }
+        else {
+          number = number + 100
+          array = array.splice(1)
+        }
+        break;
+      case 'L':
+        number = number + 50
+        array = array.splice(1)
+        break;
+      case 'X':
+        if (array[1] === 'C') {
+          number = number + 90
+          array = array.splice(2)
+        }
+        else if (array[1] === 'L') {
+          number = number + 40
+          array = array.splice(2)
+        }
+        else {
+          number = number + 10
+          array = array.splice(1)
+        }
+        break;
+      case 'V':
+        number = number + 5
+        array = array.splice(1) 
+        break;
+      case 'I':
+        if (array[1] === 'X') {
+          number = number + 9
+          array = array.splice(2)
+        }
+        else if (array[1] === 'V') {
+          number = number + 4
+          array = array.splice(2)
+        }
+        else {
+          number = number + 1
+          array = array.splice(1)
+        }
+        break;
+      default:
+        console.log("This is the default")
+      
+      
+    }
+
+    return helper(array)
+  }
+
+  return helper(number_array)
+  
 }
 
 if (require.main === module) {
